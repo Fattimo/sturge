5bd2bc984db8306bd2e6c5ede610b39dc3eeb388
Fattimo <matt.chen522@gmail.com>
2025-06-07 15:53:37 -0700

    swap to server

--- src/routes/about/+page.server.ts
+++ src/routes/about/+page.server.ts
@@ -1,6 +1,6 @@
-import type { PageLoad } from './$types';
+import type { PageServerLoad } from './$types';
 
-export const load: PageLoad = async ({ fetch }) => {
+export const load: PageServerLoad = async ({ fetch }) => {
 	const url = `https://www.random.org/integers/?num=3&min=1&max=100&col=1&base=10&format=plain&rnd=new`;
 
 	const response = await fetch(url);
