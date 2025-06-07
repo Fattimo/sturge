13a84c634894c6a79f5f4dac87505eac4250316c
Fattimo <matt.chen522@gmail.com>
2025-06-07 15:58:56 -0700

    try no csr

--- src/routes/about/+page.server.ts
+++ src/routes/about/+page.server.ts
@@ -1,4 +1,7 @@
 import type { PageServerLoad } from './$types';
+import { dev } from '$app/environment';
+
+export const csr = dev;
 
 export const load: PageServerLoad = async ({ fetch }) => {
 	const url = `https://www.random.org/integers/?num=3&min=1&max=100&col=1&base=10&format=plain&rnd=new`;
