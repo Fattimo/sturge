4e6237f00a87fd834b2234ce3d6caf1efd69604d
Fattimo <matt.chen522@gmail.com>
2025-08-29 18:08:59 -0700

    update calendar link

--- src/constants.ts
+++ src/constants.ts
@@ -5,7 +5,7 @@ export const STURGE_CHANNEL_ID = 'UCAAKEN_t0O0DA2OToKlpljg';
 
 // These need to be modified in production environments
 export const ALL_CALENDAR_LINK =
-	'https://calendar.google.com/calendar/u/0?cid=ODg0ZjAzODgzMjJhNGRlNzg3NDllNTZjNzNkYmNjMjRmZGM4MWE0Yzg3NTVjOTk5NTMyNzUwZjUwYmQ1YzVkMEBncm91cC5jYWxlbmRhci5nb29nbGUuY29t';
+	'https://calendar.google.com/calendar/embed?src=c_7aa5ae0f846653009c342d8bbda0c301068a7e34d1256bca81400c703fed2fc6%40group.calendar.google.com&ctz=America%2FLos_Angeles';
 export const FEATURED_CALENDAR_ID =
-	'884f0388322a4de78749e56c73dbcc24fdc81a4c8755c999532750f50bd5c5d0@group.calendar.google.com';
+	'c_7aa5ae0f846653009c342d8bbda0c301068a7e34d1256bca81400c703fed2fc6@group.calendar.google.com';
 export const EMAIL_ACCESS_KEY = 'b7687713-373a-47bc-aead-650d3987eb01';
