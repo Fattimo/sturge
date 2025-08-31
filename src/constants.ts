2ccf86dee5f46f703dc59f45ecfb0b30d1058cca
Fattimo <matt.chen522@gmail.com>
2025-08-30 21:25:46 -0700

    SWAP ALL CAL TO ALL CAL

--- src/constants.ts
+++ src/constants.ts
@@ -5,7 +5,7 @@ export const STURGE_CHANNEL_ID = 'UCAAKEN_t0O0DA2OToKlpljg';
 
 // These need to be modified in production environments
 export const ALL_CALENDAR_LINK =
-	'https://calendar.google.com/calendar/embed?src=c_7aa5ae0f846653009c342d8bbda0c301068a7e34d1256bca81400c703fed2fc6%40group.calendar.google.com&ctz=America%2FLos_Angeles';
+	'https://calendar.google.com/calendar/embed?src=c_a123bcb9a3eb6e25dd22b13613e4fe2441c3c0b3d61504a36a0e4001610d8cd0%40group.calendar.google.com&ctz=America%2FLos_Angeles';
 export const FEATURED_CALENDAR_ID =
 	'c_7aa5ae0f846653009c342d8bbda0c301068a7e34d1256bca81400c703fed2fc6@group.calendar.google.com';
 export const EMAIL_ACCESS_KEY = 'b7687713-373a-47bc-aead-650d3987eb01';
