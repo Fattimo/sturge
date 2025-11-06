501834dbc0707d394b191fb1047e3244b411c1c5
Fattimo <matt.chen522@gmail.com>
2025-11-05 19:07:30 -0800

    swap to email key with admin

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,5 @@
+import { dev } from '$app/environment';
+
 export const ENGLISH_ZOOM_LINK = 'https://us06web.zoom.us/j/82465688368';
 export const GOOGLE_MAPS_LINK = 'https://maps.app.goo.gl/ttgJf9wqB37656627';
 export const YOUTUBE_LINK = 'https://www.youtube.com/@sturgechurch';
@@ -8,4 +10,6 @@ export const ALL_CALENDAR_LINK =
 	'https://calendar.google.com/calendar/embed?src=c_a123bcb9a3eb6e25dd22b13613e4fe2441c3c0b3d61504a36a0e4001610d8cd0%40group.calendar.google.com&ctz=America%2FLos_Angeles';
 export const FEATURED_CALENDAR_ID =
 	'c_7aa5ae0f846653009c342d8bbda0c301068a7e34d1256bca81400c703fed2fc6@group.calendar.google.com';
-export const EMAIL_ACCESS_KEY = 'b7687713-373a-47bc-aead-650d3987eb01';
+const DEV_EMAIL_KEY = 'b7687713-373a-47bc-aead-650d3987eb01';
+const STURGE_EMAIL_KEY = '8975add1-d113-4d78-b23a-d0134e8a4af5';
+export const EMAIL_ACCESS_KEY = dev ? DEV_EMAIL_KEY : STURGE_EMAIL_KEY;
